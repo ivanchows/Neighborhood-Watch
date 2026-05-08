2af3954ba1e58b42c9e56fb4ed69203e7f5d8479
OdinTree11 <wcrabtre@stevens.edu>
2026-05-08 00:53:40 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// Here you will import route files and export the constructor method as shown in lecture code and worked in previous labs.
+
+import authRoutes from './auth_routes.js';
+const constructorMethod = (app) => {
+    app.use('/', authRoutes);
+    app.use(/(.*)/, (req, res) => {
+        res.status(404).json({ error: 'Not found' });
+    });
+};
+export default constructorMethod;
