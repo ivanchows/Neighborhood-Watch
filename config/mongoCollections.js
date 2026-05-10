65c7502d606f7d507056e6bc7a2d1944bbb16f60
chayduk <clhayduk@icloud.com>
2026-05-09 21:02:41 -0400

    Add files via upload

---
+++ config/mongoCollections.js
@@ -0,0 +1,22 @@
+import {dbConnection} from './mongoConnection.js';
+
+/* This will allow you to have one reference to each collection per app */
+/* Feel free to copy and paste this this */
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+/* Now, you can list your collections here:
+NOTE: YOU WILL NEED TO CHANGE THE CODE BELOW TO HAVE THE COLLECTION(S) REQUIRED BY THE ASSIGNMENT */
+export const incident = getCollectionFn('incidents');
+export const incident = getCollectionFn('notifications');
+export const incident = getCollectionFn('comments');
